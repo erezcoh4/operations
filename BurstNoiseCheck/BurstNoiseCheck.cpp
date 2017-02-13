d6810ca84e7fc5e9390d77c62df872ff61fb9aea
Erez Cohen <cohen.erez7@gmail.com>
2017-02-13 16:43:08 -0600

    Mon Feb 13 16:43:08 CST 2017

--- BurstNoiseCheck/BurstNoiseCheck.cpp
+++ BurstNoiseCheck/BurstNoiseCheck.cpp
@@ -117,7 +117,7 @@ int main() {
         for (size_t i_ar = 0, size_allrawdigits = rawdigit_handle->size(); i_ar != size_allrawdigits; ++i_ar) {
             int chanNum = allrawdigits_vec.at(i_ar).Channel(); // chanNum matches the wire to the channel number
             int wire = chanNum;
-            if (i_ar%200==0) std::cout << "processed "<< setprecision(1) << (100.0*float(i_ar)/Nwires) << " % of wires in event " << fevent << " (wire " << wire << ")" << endl;
+            if (i_ar%200==0) std::cout << "processed "<< setprecision(2) << (100.0*float(i_ar)/Nwires) << " % of wires in event " << fevent << " (wire " << wire << ")" << endl;
             waveform_wire[event_index][wire] -> SetName(Form("h_wf_r%d_s%d_e%d_wire%d",frun,fsubrun,fevent,(int)wire));
             waveform_wire[event_index][wire] -> SetTitle(Form("r-%d/s-%d/e-%d, wire %d wave-form; time ;ADC",frun,fsubrun,fevent,(int)wire));
             
@@ -131,11 +131,11 @@ int main() {
         
         auto t_end = high_resolution_clock::now();
         duration<double,std::milli> time_total_ms(t_end-t_begin);
-        cout << "\t run" << frun  << " ( even " << event_index << ") "<< "processes. it took me " << time_total_ms.count() << " ms to process." << endl;
+        cout << "\t run" << frun  << " (event " << event_index << ") done. it took me " << time_total_ms.count() << " ms to process." << endl;
         event_index ++ ;
         if (event_index>MaxNevents) break;
     }
-    
+    Printf("writing tree into file...");
     f_output.Write();
     f_output.Close();
     cout << "done." << endl;
